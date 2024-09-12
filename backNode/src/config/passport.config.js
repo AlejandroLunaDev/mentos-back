8c4adf89c3593163d35646391bbc558db6afa2cc
AlejandroLunaDev <alejandrolunadev@gmail.com>
2024-09-12 17:14:34 -0300

    fix: callback goolge

--- backNode/src/config/passport.config.js
+++ backNode/src/config/passport.config.js
@@ -44,7 +44,7 @@ const initializePassport = () => {
       {
         clientID: config.GOOGLE_CLIENT_ID,
         clientSecret: config.GOOGLE_CLIENT_SECRET,
-        callbackURL: config.GOOGLE_CALLBACK_URL,
+        callbackURL: config.GOOGLE_CALLBACK_URL_PROD,
         scope: ['profile', 'email'],
       },
       async (accessToken, refreshToken, profile, done) => {

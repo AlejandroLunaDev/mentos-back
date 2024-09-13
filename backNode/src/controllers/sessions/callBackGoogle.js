ec00d7432ebeda355aec786a642c4fa60d02e7c9
AlejandroLunaDev <alejandrolunadev@gmail.com>
2024-09-12 21:01:19 -0300

    out cookies

--- backNode/src/controllers/sessions/callBackGoogle.js
+++ backNode/src/controllers/sessions/callBackGoogle.js
@@ -7,8 +7,8 @@ const callbackGoogle = async (req, res) => {
     const user = req.user; // El usuario ya está autenticado con Google
     await updateUser(user._id, { last_connection: new Date() });
 
-    // Construimos el objeto userLimited según el rol
-    let userLimited = {
+    // Construimos el objeto userLimited
+    const userLimited = {
       id: user._id,
       first_name: user.first_name,
       last_name: user.last_name,
@@ -18,9 +18,9 @@ const callbackGoogle = async (req, res) => {
       age: user.age,
     };
 
+    // Si el usuario es mentor, añadimos campos adicionales
     if (user.role === 'mentor') {
-      userLimited = {
-        ...userLimited,
+      Object.assign(userLimited, {
         skills: user.skills,
         category: user.category,
         description: user.description,
@@ -28,37 +28,21 @@ const callbackGoogle = async (req, res) => {
         time_applied: user.time_applied,
         experience: user.experience,
         education: user.education,
-      };
+      });
     }
 
     // Generar token JWT
     const token = generateJWT(userLimited);
 
-    if (process.env.NODE_ENV === 'development') {
-      // En local, puedes enviar el token en una cookie
-      const cookieOptions = {
-        sameSite: 'Lax',
-        secure: false,
-        maxAge: 24 * 60 * 60 * 1000, // 1 día
-      };
-      res.cookie(config.PASS_COOKIE, token, cookieOptions);
-
-      // Redirigir al frontend local en desarrollo
-      return res.status(200).redirect('http://localhost:5173/');
-    }
-
-    // En producción, enviar el token en JSON
-    if (process.env.NODE_ENV === 'production') {
-      return res.status(200).json({
-        message: 'Inicio de sesión exitoso',
-        token,  // Enviar el token en el cuerpo de la respuesta
-        redirectURL: user.role === 'admin' 
-          ? 'https://mentos.com/admin' 
-          : 'https://mentos.com/', // URL adecuada para producción
-      });
-    }
+    // Enviar el token en JSON, independientemente del entorno
+    return res.status(200).json({
+      message: 'Inicio de sesión exitoso',
+      token,  // Enviar el token en el cuerpo de la respuesta
+      redirectURL: 'http://localhost:5173/', // URL base del frontend
+    });
+    
   } catch (error) {
-    console.error(error);
+    console.error('Error en el callback de Google:', error);
     return res.status(500).send('Error al iniciar sesión con Google');
   }
 };

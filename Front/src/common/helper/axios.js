7ac3d96f5f2aff089f30699eedb7e121c4610bc2
OmarnLujan <127269393+OmarnLujan@users.noreply.github.com>
2024-09-16 17:39:26 -0300

    fix

--- Front/src/common/helper/axios.js
+++ Front/src/common/helper/axios.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 import { getBaseUrl } from "../../helper/envHelper";
-const BASE_URL = getBaseUrl();
-console.log(BASE_URL);
-// axios.defaults.baseURL = "http://localhost:3000";
-axios.defaults.baseURL = BASE_URL;
+
+axios.defaults.baseURL = getBaseUrl();
 
 export default axios;
